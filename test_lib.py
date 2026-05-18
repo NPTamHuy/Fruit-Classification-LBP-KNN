69b10aa55ac0e329a38ec8d371338fb11be190ca
TamHuy <nptamhuy93@gmail.com>
2026-05-18 09:51:41 +0700

    add src

---
+++ test_lib.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.feature import local_binary_pattern
+from sklearn.neighbors import KNeighborsClassifier
+
+print("Tat ca thu vien da cai thanh cong!")
