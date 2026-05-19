da623d0ff89d09587505f9f234ab85fbbd8f5028
TamHuy <nptamhuy93@gmail.com>
2026-05-19 12:55:43 +0700

    update requirements.txt

--- test_lib.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-from skimage.feature import local_binary_pattern
-from sklearn.neighbors import KNeighborsClassifier
-
-print("Tat ca thu vien da cai thanh cong!")
